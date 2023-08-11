28cf5a66ded1455c81526a3c3d74ea30da9a7f66
Arda <boz.arda33@gmail.com>
2023-08-11 19:43:51 +0300

    html 1

---
+++ src/Gun04/_01_OppeningWebsite.java
@@ -0,0 +1,21 @@
+package Gun04;
+
+import Utility.MyFunc;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _01_OppeningWebsite {
+    public static void main(String[] args) {
+
+        // oop WebDriver: Interface,Parent class  ChromeDriver,firefox,EdgeDriver: child class
+        WebDriver driver=new ChromeDriver(); // driver = browser, tarayıcı
+        // WebDriver driver2= new FirefoxDriver();
+        // WebDriver driver3= new EdgeDriver();
+
+
+        driver.get("https://techno.study/tr"); // web sayfasını açma
+
+        MyFunc.Wait(3);
+        driver.quit();
+    }
+}
